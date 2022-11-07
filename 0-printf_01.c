6066e011b7554d73483b49dba7ec7e0bf90b517a
Leakim01 <5662@holbertonstudents.com>
2022-11-07 03:14:11 -0800

    Task 0 - Function that Produces Output According

---
+++ 0-printf_01.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * _printf: Prototype
+ * format: Paramter 1
+ *
+ * Return: O
+ *
+ */
+
+
---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <locale.h>
+#include <string.h>
+#include <stdarg.h>
+#include <limits.h>
+
+int _printf(const char *format, ...);
+int _putchar(char ch);
+#endif
