116cab8917c8e23f5e1321752a1dfd8276cb84df
Amandine4731 <5618@holbertonstudents.com>
2022-11-11 17:34:51 -0800

    Task 1 - printf %c %s %% %d %i

--- get_functions_printf.c
+++ get_functions_printf.c
@@ -7,18 +7,18 @@
  */
 int get_functions_printf(va_list mylist, char s)
 {
+	int i;
 	var_t vars[] = {
 		{'c', var_c},
 		{'s', var_s},
 		{'d', var_i},
 		{'i', var_i},
-		{0, NULL}
 	};
-	int i = 0;
+	i = 0;
 
 	while (vars[i].var)
 	{
-		if (s == vars[i].var)
+		if (vars[i].var == s)
 		{
 			return (vars[i].f(mylist));
 		}
@@ -26,5 +26,5 @@ int get_functions_printf(va_list mylist, char s)
 	}
 	putchar('%');
 	putchar(s);
-	return (i);
+	return (2);
 }
